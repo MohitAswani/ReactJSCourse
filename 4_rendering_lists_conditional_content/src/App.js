30256afbaf3003d25a904169c572384b771c3ee9
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-07-07 15:34:07 +0530

    Adding code.

--- 4_rendering_lists_conditional_content/src/App.js
+++ 4_rendering_lists_conditional_content/src/App.js
@@ -3,43 +3,10 @@ import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Car insurance",
-    amount: 299.99,
-    date: new Date(2020, 2, 28),
-  },
-  {
-    id: "e2",
-    title: "Toilet paper",
-    amount: 29.99,
-    date: new Date(2022, 5, 26),
-  },
-  {
-    id: "e3",
-    title: "Gun",
-    amount: 199.99,
-    date: new Date(2022, 1, 30),
-  },
-  {
-    id: "e4",
-    title: "Grocery",
-    amount: 599.99,
-    date: new Date(2021, 7, 31),
-  },
-  {
-    id: "e5",
-    title: "Home insurance",
-    amount: 299.99,
-    date: new Date(2022, 4, 18),
-  },
-];
-
 const App = () => {
 
   // Stateful lists
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState([]);
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
