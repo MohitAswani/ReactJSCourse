e72da554b3fa5cdc1c119191fe28c10a0c1f2dc6
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-07-17 17:13:06 +0530

    Adding code.

---
+++ 10_food_order_app/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import mealsImg from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Bromato</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="Food img" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
